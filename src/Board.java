ebcb29de89ad14771e67bfeae841c9cc536142d2
ArpanUprety <114251047+ArpanUprety@users.noreply.github.com>
2023-11-07 14:41:49 -0500

    Another Commit

--- src/Board.java
+++ src/Board.java
@@ -1,7 +1,11 @@
 import java.util.ArrayList;
 public class Board {
+    private int gazzoRow;
+    private int gazzoCol;
+    private  int maxrow;
+    private int maxcol;
     ArrayList<ArrayList<Space>> Board = new ArrayList<ArrayList<Space>>();
-
+    LivingThing player = new LivingThing("Gazoo", 20, ConsoleColors.GREEN);
     private int rows = 0;
     private   int columns = 0;
     public Board(){
@@ -12,11 +16,15 @@ public class Board {
         this.columns = columns;
         this.rows = rows;
         buildBoard();
-        LivingThing player = new LivingThing("Gazoo", 20, ConsoleColors.GREEN);
-        Board.get(0).get(0).setOccupant(player);
+
+        gazzoRow = 0;
+        gazzoCol = 0;
+        Board.get(gazzoRow).get(gazzoCol).setOccupant(player);
     }
 
     private void buildBoard(){
+        maxrow = rows -1;
+       maxcol = columns -1;
         for(int i = 0; i < rows; i++){
             ArrayList<Space> row = new ArrayList<>();
             for(int n = 0; n < columns; n++){
@@ -37,5 +45,98 @@ public class Board {
             System.out.println(result);
 
 
+
     }
+public  boolean move(char m){
+
+
+        switch (m){
+            case 'a' :
+                if (validSpace(gazzoRow  ,gazzoCol -1)){
+                    Board.get(gazzoRow).get(gazzoCol).setOccupant(null);
+                    gazzoCol -=1;
+                    Board.get(gazzoRow ).get(gazzoCol).setOccupant(player);
+
+                }
+break;
+
+
+            case 'w':
+                if (validSpace(gazzoRow -1 ,gazzoCol)){
+                    Board.get(gazzoRow).get(gazzoCol).setOccupant(null);
+                    gazzoRow -=1;
+                    Board.get(gazzoRow ).get(gazzoCol).setOccupant(player);
+
+                }
+                break;
+            case 's':
+    if (validSpace(gazzoRow +1 ,gazzoCol)){
+        Board.get(gazzoRow).get(gazzoCol).setOccupant(null);
+        gazzoRow +=1;
+        Board.get(gazzoRow ).get(gazzoCol).setOccupant(player);
+
+    }
+                break;
+            case 'd':
+              if (validSpace(gazzoRow,gazzoCol + 1)){
+                  Board.get(gazzoRow).get(gazzoCol).setOccupant(null);
+                  gazzoCol +=1;
+                  Board.get(gazzoRow ).get(gazzoCol).setOccupant(player);
+            }
+                break;
+            default:
+                return false;
+
+        }
+
+    return false;
+}
+
+
+public boolean validSpace(int row, int col){
+        return row >= 0 && row < Board.size() && col >= 0 && col < Board.get(row).size();
+}
+
+public boolean validSpaceL(int row){
+        if (gazzoRow != 0 ) {
+
+         return true;
+        }else {
+            System.out.println("can't move in this direction ");
+            return false;
+        }
+}
+    public boolean validSpaceR(int col){
+        if ( gazzoCol >= maxcol-1) {
+            System.out.println("can't move in this direction ");
+            return false;
+        }else {
+
+            return true;
+        }
+    }
+
+
+
+    public boolean validSpaceW( int col){
+        if (gazzoCol != 0 ) {
+
+            return true;
+        }else {
+            System.out.println("can't move in this direction ");
+            return false;
+        }
+    }
+
+
+    public boolean validSpaceS(int rows){
+        if ( gazzoCol >= maxrow -1) {
+            System.out.println("can't move in this direction ");
+            return false;
+        }else {
+
+            return true;
+        }
+    }
+
 }
--- src/BoardDriver1.java
+++ src/BoardDriver1.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class BoardDriver1 {
 
+
+
     public static void main(String[] args) {
-        Board b = new Board();
+       Scanner input = new Scanner(System.in);
+         char i;
+        Board b2 = new Board(8, 9);
+        do {
+            b2.printBoard();
+            System.out.print("To move Gazoo, type the character w to move up, a to move left, s to move down, or d to move right! ->  ");
+            i = input.next().charAt(0);
+            b2.move(i);
+            //prompt the user for w s
+            // w a s d
+            //pass that as a char to move
+        } while (true);
 
-        b.printBoard();
 
-        Board b2 = new Board(5, 6);
 
-        b2.printBoard();
+
     }
 
 }
